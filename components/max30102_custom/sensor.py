50780b93774f622df00c0658baf10e7e07f03a0a
miroslav-milos <miroslav.milos93@gmail.com>
2026-02-03 12:10:29 +0100

    Update sensor.py

--- components/max30102_custom/sensor.py
+++ components/max30102_custom/sensor.py
@@ -1,1 +1,10 @@
-# all implemented in __init__.py (placeholder)
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome import core
+
+DEPENDENCIES = []
+AUTO_LOAD = ["sensor"]
+
+max30102_ns = cg.esphome_ns.namespace("max30102_custom")
+MAX30102CustomSensor = max30102_ns.class_("MAX30102CustomSensor", sensor.Sensor)# all implemented in __init__.py (placeholder)
